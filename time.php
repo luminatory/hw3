3137a57c4a5f7026f3832473fc7a892ec7949773
luminatory <luminatory@users.noreply.github.com>
2015-05-16 11:00:12 +0300

    Update time.php

--- time.php
+++ time.php
@@ -4,21 +4,21 @@ function CheckTime($value){
 
 
 $time = explode(":", $value);
-switch($time){
+$result = false;
 case (($time[0]>=5 && $time[0]<=10) && ($time[1]>=0 && $time[1]<=59)):
-	echo 'Good morning!<br />';
+	$result = 'Good morning!<br />';
 	break;
 case (($time[0]>=11 && $time[0]<=16) && ($time[1]>=0 && $time[1]<=59)):
-	echo 'Good afternoon!';
+	$result = 'Good afternoon!';
 	break;
 case (($time[0]>=17 && $time[0]<=21) && ($time[1]>=0 && $time[1]<=59)):
-	echo 'Good everning!<br />';
+	$result = 'Good everning!<br />';
 	break;
 case (($time[0]>=1 && $time[0]<=4) && ($time[1]>=0 && $time[1]<=59)):
-	echo 'Good Night!<br />';
+	$result = 'Good Night!<br />';
 	break;
 }
-
+return $result;
 	
 	}
 
